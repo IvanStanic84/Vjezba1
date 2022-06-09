3fbf935f6ac028852b7c4c99f5cce914fb4b20ed
Korisnik <stambaivan@yahoo.com>
2022-06-09 21:19:44 +0200

    ghj

---
+++ src/vjezba/Vjezba1.java
@@ -0,0 +1,19 @@
+package vjezba;
+
+import javax.swing.JOptionPane;
+
+public class Vjezba1 {
+
+	public static void main(String[] args) {
+
+		int prviBroj = Integer.parseInt(JOptionPane.showInputDialog("Unesi prvi broj"));
+
+		int drugiBroj = Integer.parseInt(JOptionPane.showInputDialog("Unesi drugi broj"));
+
+		int rezultat = prviBroj + drugiBroj;
+
+		System.out.println(rezultat);
+
+	}
+
+}
