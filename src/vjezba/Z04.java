d6d29aac49eed6c146f6848cecc6cd68a2295e70
Korisnik <stambaivan@yahoo.com>
2022-06-14 20:35:55 +0200

    rijeseno

--- src/vjezba/Z04.java
+++ src/vjezba/Z04.java
@@ -1,10 +1,21 @@
 package vjezba;
 
+import javax.swing.JOptionPane;
+
 public class Z04 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int i = Integer.parseInt(JOptionPane.showInputDialog("unesi prvi broj"));
+		
+		int j = Integer.parseInt(JOptionPane.showInputDialog("unesi prvi broj"));
 
+		int z = i + j;
+		if(z>10) {
+			System.out.println("Osijek");
+		}else {
+			System.out.println("Edunova");
+		}
 	}
 
 }
