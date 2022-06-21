2d7a64bd4d64110603dd73c6f7cc3468dfac5376
Korisnik <stambaivan@yahoo.com>
2022-06-21 19:15:15 +0200

    rijeseno

---
+++ src/vjezba/Vjezba6.java
@@ -0,0 +1,17 @@
+package vjezba;
+
+public class Vjezba6 {
+
+	public static void main(String[] args) {
+
+		int[][] matrica = new int[10][10];
+
+		matrica[9][9] = 8;
+
+		int[][] tablica = { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } };
+
+		int[][][] kocka = new int[10][10][10];
+
+	}
+
+}
