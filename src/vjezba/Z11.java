341c289b5c38cfe74d0e5fc889ae7b15c1e0d472
Korisnik <stambaivan@yahoo.com>
2022-06-21 21:14:24 +0200

    rijeseno

--- src/vjezba/Z11.java
+++ src/vjezba/Z11.java
@@ -1,22 +1,27 @@
 package vjezba;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
+import javax.swing.JOptionPane;
 
 public class Z11 {
 
 	public static void main(String[] args) {
-		int d = 10;
-		
-		int[][] tablica = new int[d][d];
-		for (int i = 0; i < d; i++) {
-			for (int j = 0; j < d; j++) {
-				tablica[i][j] = (i + 1) * (j + 1);
+
+		int i;
+
+		for (;;) {
+			i = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj izmeðu 10 i 20"));
+			if (i < 10 || i > 20) {
+				break;
+			}
+			int k = 1;
+			int l = 1;
+
+			for (k = 1; k <= 10; k++) {
+				for (l = 1; l <= 10; l++)
+					System.out.printf("%4d", k * l);
+				System.out.println();
 			}
 		}
 
-		for(int i=0;i<d;i++) {
-			System.out.println(Arrays.toString(tablica[i]));
-		}
 	}
 }
