f91c9c416cb7cc449155f8a8f367c0d53c4efc35
Korisnik <stambaivan@yahoo.com>
2022-06-21 19:15:06 +0200

    rijeseno

--- src/vjezba/Vjezba5.java
+++ src/vjezba/Vjezba5.java
@@ -7,39 +7,32 @@ import javax.swing.JOptionPane;
 public class Vjezba5 {
 
 	public static void main(String[] args) {
-		
+
 		String s = JOptionPane.showInputDialog("Unesi svoje ime");
-		
+
 		System.out.println(s.charAt(0));
-		System.out.println(s.charAt(s.length()-1));
+		System.out.println(s.charAt(s.length() - 1));
 		System.out.println();
-		
-		
+
 		String ime = new String();
-		
+
 		String grad = "Osijek";
-		
+
 		System.out.println(grad.hashCode());
-		
+
 		System.out.println(grad.hashCode());
-		
-		grad="Valpovo";
-		
+
+		grad = "Valpovo";
+
 		System.out.println(grad.hashCode());
-		
+
 		String[] parametri = new String[12];
-		
-		parametri[0]= "Edunova";
-		parametri[1]= new String("Osijek");
-		
+
+		parametri[0] = "Edunova";
+		parametri[1] = new String("Osijek");
+
 		System.out.println(Arrays.toString(parametri));
 		System.out.println(parametri[1].charAt(1));
-		
-		
-		
-		
-		
-	
 
 	}
 
