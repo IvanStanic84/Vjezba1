6fcfaade3b2d9f005fe92533114704ec44a82da3
Korisnik <stambaivan@yahoo.com>
2022-06-21 19:07:13 +0200

    rijeseno

--- src/vjezba/Vjezba4.java
+++ src/vjezba/Vjezba4.java
@@ -7,23 +7,21 @@ public class Vjezba4 {
 	public static void main(String[] args) {
 
 		int[] temp;
-		
+
 		temp = new int[12];
-		
-		temp[0]=2;
-		temp[1]=1;
-		temp[temp.length-1]=5;
-		
+
+		temp[0] = 2;
+		temp[1] = 1;
+		temp[temp.length - 1] = 5;
+
 		System.out.println(Arrays.toString(temp));
-		
-		int boje[]= new int[4];
-		boje[1]=7;
+
+		int boje[] = new int[4];
+		boje[1] = 7;
 		System.out.println(boje[1]);
-		
-		int[] vrijednosti= {2,5,6,3,4,1,5,1,5,2};
-		System.out.println(vrijednosti[0] + "," + vrijednosti[vrijednosti.length-1]);
-		
-		
+
+		int[] vrijednosti = { 2, 5, 6, 3, 4, 1, 5, 1, 5, 2 };
+		System.out.println(vrijednosti[0] + "," + vrijednosti[vrijednosti.length - 1]);
 
 	}
 
