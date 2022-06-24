565ac1b60b9b4f81fe7a04d4d655c8563ee10f95
Korisnik <stambaivan@yahoo.com>
2022-06-24 08:26:19 +0200

    primjeri

---
+++ src/vjezba/Vjezba8.java
@@ -0,0 +1,28 @@
+package vjezba;
+
+public class Vjezba8 {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 10; i = i + 1) {
+			System.out.println("Osijek");
+		}
+
+		for (int i = 0; i < 10; i += 1) {
+			System.out.println(i + 1);
+		}
+
+		for (int i = 10; i > 0; i = i - 1) {
+			System.out.println(i);
+		}
+
+		for (int i = 0; i < 100; i = i + 10) {
+			System.out.println(i);
+		}
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
