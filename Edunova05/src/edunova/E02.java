77291edd3485c55e958fa8d529008b09a2f1b64b
Korisnik <stambaivan@yahoo.com>
2022-06-28 11:04:20 +0200

    predavanja

---
+++ Edunova05/src/edunova/E02.java
@@ -0,0 +1,18 @@
+package edunova;
+
+public class E02 {
+
+	public static void main(String[] args) {
+
+		int i = 10;
+		do {
+			System.out.println("Osijek");
+		} while (i > 10);
+
+		do {
+			break;
+		} while (true);
+
+	}
+
+}
