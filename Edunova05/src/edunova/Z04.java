5b636e525f97cfdaaf6c47e184d17369db16053b
Korisnik <stambaivan@yahoo.com>
2022-06-28 19:56:28 +0200

    zbroj znamenaka

---
+++ Edunova05/src/edunova/Z04.java
@@ -0,0 +1,27 @@
+package edunova;
+
+import javax.swing.JOptionPane;
+
+public class Z04 {
+
+	public static void main(String[] args) {
+
+		// ispiši zbroj zvih znamenki
+		// ulaz: 263736
+		// izlaz: 27
+		// ulaz: 26
+		// izlaz: 8
+
+		int i = 0;
+		int suma = 0;
+		i = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj"));
+
+		while (i > 0) {
+			suma += i % 10;
+
+			i = i / 10;
+		}
+		System.out.println(suma);
+	}
+
+}
