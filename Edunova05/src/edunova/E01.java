a4e3276f7d11eab4f2baf896f1e788692289a4dd
Korisnik <stambaivan@yahoo.com>
2022-06-28 11:17:52 +0200

    predavanja

--- Edunova05/src/edunova/E01.java
+++ Edunova05/src/edunova/E01.java
@@ -3,37 +3,29 @@ package edunova;
 public class E01 {
 
 	public static void main(String[] args) {
-		
-	
-			while(true) {
-				break;
-			}
-			
-			
-			
-			int i = 0;
-			while(i<10) {
-				if(i+1==2) {
-					i++;
-					continue;
-				}
-				System.out.println(++i);
-			}
-			
-		
-			for(int t=10;t>10;t++) {
-				System.out.println("Osijek");
-			}
-			
-		
-			int t=2;
-			while(t>2) {
-				System.out.println("Edunova");
-			}
-			
-			
+
+		while (true) {
+			break;
 		}
-		
+
+		int i = 0;
+		while (i < 10) {
+			if (i + 1 == 2) {
+				i++;
+				continue;
+			}
+			System.out.println(++i);
+		}
+
+		for (int t = 10; t > 10; t++) {
+			System.out.println("Osijek");
+		}
+
+		int t = 2;
+		while (t > 2) {
+			System.out.println("Edunova");
+		}
+
 	}
 
-
+}
