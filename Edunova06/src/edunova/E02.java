0def63d6cb0fe1a6ed0c0951f3fa63be35640e31
Korisnik <stambaivan@yahoo.com>
2022-07-02 14:51:38 +0200

    Rekurzija

--- Edunova06/src/edunova/E02.java
+++ Edunova06/src/edunova/E02.java
@@ -2,4 +2,20 @@ package edunova;
 
 public class E02 {
 
+	public static void main(String[] args) {
+		// metoda();
+		System.out.println(zbroj(100));
+	}
+	// java.lang.StackOverflowError
+	// static void metoda() {
+	// metoda();
+	// }
+
+	static int zbroj(int broj) {
+		if (broj == 1) {
+			return 1;
+		}
+		return broj + zbroj(broj - 1);
+	}
+
 }
