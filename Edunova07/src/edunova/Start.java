a1054f1870ced45b03dac735d487352a82612aad
Korisnik <stambaivan@yahoo.com>
2022-07-05 18:53:08 +0200

    start

--- Edunova07/src/edunova/Start.java
+++ Edunova07/src/edunova/Start.java
@@ -1,5 +1,44 @@
 package edunova;
 
+import javax.swing.JButton;
+
 public class Start {
 
+	// ÈITATI
+	// https://github.com/tjakopec/OOP_JAVA_PHP_PYTHON_SWIFT
+
+	public static void main(String[] args) {
+
+		// 1. princip OOP-a Klasa/objekt
+		// osoba je varijabla instance = objekt
+		// Instanciram (nova pojavnost) klase
+		// tipa Osoba pod imenom objekta osoba
+		Osoba osoba = new Osoba();
+		// osoba.sifra=1; OVO SE NE RADI
+		osoba.setSifra(1);
+		System.out.println(osoba.getSifra());
+
+		JButton b = new JButton();
+
+		Racunalo racunalo = new Racunalo();
+		racunalo.setSifra(1);
+		racunalo.setNaziv("HP");
+		racunalo.setDatumNabave(RadSaDatumima.kreirajDatum(2022, 7, 4));
+		racunalo.setIspravno(true);
+
+		System.out.println(racunalo.getNaziv());
+		System.out.println(racunalo.getDatumNabave());
+
+		Osoba[] osobe = new Osoba[30];
+		for (int i = 0; i < 30; i++) {
+			osoba = new Osoba();
+			osoba.setSifra(i + 1);
+			osobe[i] = osoba;
+		}
+
+		for (Osoba o : osobe) {
+			System.out.println(o.getSifra());
+		}
+
+	}
 }
