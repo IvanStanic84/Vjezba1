f16bd538c1f57a245a4c64d8584f47f528596b47
Korisnik <stambaivan@yahoo.com>
2022-07-05 18:55:29 +0200

    Datumi

--- Edunova07/src/edunova/RadSaDatumima.java
+++ Edunova07/src/edunova/RadSaDatumima.java
@@ -1,5 +1,25 @@
 package edunova;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 public class RadSaDatumima {
+
+	public static Date kreirajDatum(int godina, int mjesec, int dan) {
+
+		Date datum = new Date();
+		// prekriženo znaèi depricated, NE KORISTITI
+		datum.setDate(dan);
+
+		datum = new Date();
+		// vrijeme (i datum) je prema
+
+		GregorianCalendar gregorianCalendar = (GregorianCalendar) Calendar.getInstance();
+		gregorianCalendar.set(Calendar.YEAR, godina);
+		gregorianCalendar.set(Calendar.MONTH, mjesec - 1);
+		gregorianCalendar.set(Calendar.DAY_OF_MONTH, dan);
+		return gregorianCalendar.getTime();
+	}
 
 }
