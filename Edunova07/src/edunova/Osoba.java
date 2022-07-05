d4f18479dea8c35109778a27d2895dc884ce3aeb
Korisnik <stambaivan@yahoo.com>
2022-07-05 18:54:57 +0200

    osoba

--- Edunova07/src/edunova/Osoba.java
+++ Edunova07/src/edunova/Osoba.java
@@ -1,5 +1,27 @@
 package edunova;
 
 public class Osoba {
+// int sifra; OVO SE NE RADI
+
+	// 2. princip OOP-a Uèahurivanje
+
+	// naèini pristupa
+	// package scope (bez navoðenja naèina)
+	// class scope (private)
+	// class scope (protected) - nasljeðivanje
+	// vidljivo svima (public)
+
+	// Klasa sakrije svoja svojstva
+	private int sifra;
+
+	// i uèini ih dostupnim putem
+	// tkz. geter i seter metoda
+	public int getSifra() {
+		return sifra;
+	}
+
+	public void setSifra(int sifra) {
+		this.sifra = sifra;
+	}
 
 }
