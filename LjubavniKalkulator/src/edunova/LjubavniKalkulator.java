ecfee150d9aa621e60c92b4b83655fdc4f899f95
Korisnik <stambaivan@yahoo.com>
2022-07-06 15:37:58 +0200

    treci red

--- LjubavniKalkulator/src/edunova/LjubavniKalkulator.java
+++ LjubavniKalkulator/src/edunova/LjubavniKalkulator.java
@@ -4,13 +4,13 @@ public class LjubavniKalkulator {
 
 	public static void main(String[] args) {
 
-		String ime1 = "Marta";
-		String ime2 = "Manuel";
+		String ime1 = "Ivan";
+		String ime2 = "Tea";
 		String ime12 = ime1 + ime2;
 
 		int k = 0;
 		int[] niz = new int[ime12.length()];
-		System.out.println(ime12);
+		System.out.println(ime1 +"   "+ ime2);
 		for (int i = 0; i < ime12.length(); i++) {
 			k = 0;
 			for (int j = 0; j < ime12.length(); j++) {
@@ -36,18 +36,16 @@ public class LjubavniKalkulator {
 		} else {
 			for (int i = 0; i < ime12.length() / 2; i++) {
 				int prvibroj = niz[i];
-				int drugibroj = niz[ime12.length() - 1 - i];
-
-				int srednjibroj = niz[ime12.length() / 2 + 1];
+				int drugibroj = niz[ime12.length() - 1 - i];				
 				int rezultat = prvibroj + drugibroj;
 				System.out.print(rezultat + " ");
 
-				System.out.print(srednjibroj);
 
-			}
+			}	int srednjibroj = niz[ime12.length() / 2 + 1];
+				System.out.print(srednjibroj);
 
 		}
 
 	}
-	// }
+
 }
