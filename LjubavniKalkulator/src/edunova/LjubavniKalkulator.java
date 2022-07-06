69b7ea00f9ad84870f19498588c15b06f8d7ee50
Korisnik <stambaivan@yahoo.com>
2022-07-06 10:51:13 +0200

    pocetak

---
+++ LjubavniKalkulator/src/edunova/LjubavniKalkulator.java
@@ -0,0 +1,25 @@
+package edunova;
+
+public class LjubavniKalkulator {
+
+	public static void main(String[] args) {
+
+		String ime1 = "Marta";
+		String ime2 = "Manuel";
+		String ime12 = ime1 + ime2;
+		
+		int k=0;
+		int[] niz = new int[ime12.length()];
+	
+		for(k=0;k<ime12.length();k++) {
+			
+		}
+		
+		System.out.println(ime12);
+		
+		
+		
+		
+	}
+
+}
--- LjubavniKalkulator/src/module-info.java
+++ LjubavniKalkulator/src/module-info.java
@@ -1,2 +1,4 @@
 module ljubavniKalkulator {
+	requires java.desktop;
+
 }
