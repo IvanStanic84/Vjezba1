c17910b32c75e5ec7daebf63e971f7bf95dc1bb3
Korisnik <stambaivan@yahoo.com>
2022-07-06 15:55:54 +0200

    svi redovi, zasad prva 3 valjaju

--- LjubavniKalkulator/src/edunova/LjubavniKalkulator.java
+++ LjubavniKalkulator/src/edunova/LjubavniKalkulator.java
@@ -4,13 +4,13 @@ public class LjubavniKalkulator {
 
 	public static void main(String[] args) {
 
-		String ime1 = "Ivan";
-		String ime2 = "Tea";
+		String ime1 = "Marta";
+		String ime2 = "Manuel";
 		String ime12 = ime1 + ime2;
 
 		int k = 0;
 		int[] niz = new int[ime12.length()];
-		System.out.println(ime1 +"   "+ ime2);
+		System.out.println(ime1 + "   " + ime2);
 		for (int i = 0; i < ime12.length(); i++) {
 			k = 0;
 			for (int j = 0; j < ime12.length(); j++) {
@@ -25,27 +25,32 @@ public class LjubavniKalkulator {
 		System.out.println();
 		k = 0;
 
-		if (ime12.length() % 2 == 0) {
-			for (int i = 0; i < ime12.length() / 2; i++) {
-				int prvibroj = niz[i];
-				int drugibroj = niz[ime12.length() - 1 - i];
-				int rezultat = prvibroj + drugibroj;
-				System.out.print(rezultat + " ");
+
+		for (k = ime12.length() / 2; k > 2; k--) {
+
+			if (ime12.length() % 2 == 0) {
+				for (int i = 0; i < ime12.length() / 2; i++) {
+					int prvibroj = niz[i];
+					int drugibroj = niz[ime12.length() - 1 - i];
+					int rezultat = prvibroj + drugibroj;
+					System.out.print(rezultat + " ");
+					System.out.println();
+
+				}
+			} else {
+				for (int i = 0; i < ime12.length() / 2; i++) {
+					int prvibroj = niz[i];
+					int drugibroj = niz[ime12.length() - 1 - i];
+					int rezultat = prvibroj + drugibroj;
+					System.out.print(rezultat + " ");
+
+				}
+				int srednjibroj = niz[ime12.length() / 2];
+				System.out.print(srednjibroj);
+				System.out.println();
 
 			}
-		} else {
-			for (int i = 0; i < ime12.length() / 2; i++) {
-				int prvibroj = niz[i];
-				int drugibroj = niz[ime12.length() - 1 - i];				
-				int rezultat = prvibroj + drugibroj;
-				System.out.print(rezultat + " ");
-
-
-			}	int srednjibroj = niz[ime12.length() / 2 + 1];
-				System.out.print(srednjibroj);
-
 		}
-
 	}
 
 }
