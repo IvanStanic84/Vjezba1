708fb2e5a17203c90907ea3ae903766f5833f02f
Korisnik <stambaivan@yahoo.com>
2022-07-06 11:07:50 +0200

    ispisuje broj istih slova

--- LjubavniKalkulator/src/edunova/LjubavniKalkulator.java
+++ LjubavniKalkulator/src/edunova/LjubavniKalkulator.java
@@ -7,19 +7,22 @@ public class LjubavniKalkulator {
 		String ime1 = "Marta";
 		String ime2 = "Manuel";
 		String ime12 = ime1 + ime2;
-		
-		int k=0;
+
+		int k = 0;
 		int[] niz = new int[ime12.length()];
-	
-		for(k=0;k<ime12.length();k++) {
-			
-		}
-		
 		System.out.println(ime12);
-		
-		
-		
-		
+		for (int i = 0; i < ime12.length(); i++) {
+			k = 0;
+			for (int j = 0; j < ime12.length(); j++) {
+				if (ime12.charAt(i) == ime12.charAt(j)) {
+					k++;
+				}
+			}
+			niz[i] = k;
+			System.out.print(k + " ");
+
+		}
+
 	}
 
 }
