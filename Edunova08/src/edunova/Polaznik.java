3848793c78ad75a1b3486f0d9771e0c94bb32a74
Korisnik <stambaivan@yahoo.com>
2022-07-08 22:11:58 +0200

    polaznik

--- Edunova08/src/edunova/Polaznik.java
+++ Edunova08/src/edunova/Polaznik.java
@@ -1,5 +1,24 @@
 package edunova;
 
-public class Polaznik {
+public class Polaznik extends Osoba {
+
+	private String brojUgovora;
+
+	public Polaznik() {
+		super(); // poziva se konstruktor klase Osoba jer Polaznik nasljeðuje klasu Osoba
+	}
+
+	public Polaznik(String ime, String prezime, String brojUgovora) {
+		super(ime, prezime);
+		this.brojUgovora = brojUgovora;
+	}
+
+	public String getBrojUgovora() {
+		return brojUgovora;
+	}
+
+	public void setBrojUgovora(String brojUgovora) {
+		this.brojUgovora = brojUgovora;
+	}
 
 }
