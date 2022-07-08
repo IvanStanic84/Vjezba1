4b7c25ef6724c85b4b8e17b25bc7fe9474202425
Korisnik <stambaivan@yahoo.com>
2022-07-08 22:18:12 +0200

    pomocno

--- Edunova08/src/util/Pomocno.java
+++ Edunova08/src/util/Pomocno.java
@@ -1,5 +1,62 @@
 package util;
 
+import javax.swing.JOptionPane;
+
 public class Pomocno {
+
+	/**
+	 * Metoda osigurava unos cjelog broja koji može biti u cijelom rasponu int tip
+	 * podatka
+	 * 
+	 * @param poruka Koja se prikazuje korisniku
+	 * @return Uèitani cjeli broj
+	 */
+	public static int ucitajInt(String poruka) {
+		while (true) {
+			try {
+				return Integer.parseInt(JOptionPane.showInputDialog(poruka));
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(null, "Niste unijeli cijeli broj");
+			}
+		}
+	}
+
+	public static String ucitajString(String poruka) {
+		String s;
+		while (true) {
+			s = JOptionPane.showInputDialog(poruka);
+			if (s.trim().isEmpty()) {
+				JOptionPane.showMessageDialog(null, "Obavezan unos");
+				continue;
+			}
+			return s;
+		}
+	}
+
+	public static int ucitajInt(String poruka, boolean pozitivni) {
+		int i = ucitajInt(poruka);
+		if (!pozitivni) {
+			return i;
+		}
+		while (true) {
+			if (i < 0) {
+				JOptionPane.showMessageDialog(null, "Uèitani broj mora biti pozitivan");
+				i = ucitajInt(poruka);
+				continue;
+			}
+			return i;
+		}
+	}
+
+	/**
+	 * https://stackoverflow.com/questions/363681/how-do-i-generate-random-integers-within-a-specific-range-in-java
+	 * 
+	 * @param min
+	 * @param max
+	 * @return
+	 */
+	public static int slucajniBroj(int min, int max) {
+		return min + (int) (Math.random() * ((max - min) + 1));
+	}
 
 }
