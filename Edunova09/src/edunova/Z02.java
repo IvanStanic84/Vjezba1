e8ffab0a60afeb557724bbb1c0188b181cc83c46
Korisnik <stambaivan@yahoo.com>
2022-07-10 19:13:16 +0200

    aritmeticka sredina

---
+++ Edunova09/src/edunova/Z02.java
@@ -0,0 +1,35 @@
+package edunova;
+
+import javax.swing.JOptionPane;
+
+public class Z02 {
+	public static void main(String[] args) {
+
+		String broj = "";
+		int x;
+		int suma = 0;
+		int countUnosa = 0;
+
+		while (true) {
+			try {
+				broj = JOptionPane.showInputDialog("Unesi broj: ");
+
+				x = Integer.parseInt(broj);
+
+				suma += x;
+				countUnosa++;
+
+			} catch (Exception e) {
+
+				if (broj.toLowerCase().trim().equals("stop")) {
+					break;
+
+				} else {
+					JOptionPane.showMessageDialog(null, "Obavezan unos ili stop za kraj");
+				}
+
+			}
+		}
+		System.out.println(suma / (double) countUnosa);
+	}
+}
