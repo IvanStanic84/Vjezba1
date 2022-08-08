a888a833846c4d1bc8853d061740b97a1275008c
Korisnik <stambaivan@yahoo.com>
2022-08-08 20:24:45 +0200

    rekurzija

--- LjubavniKalkulator/src/edunova/LjubavniKalkulator.java
+++ LjubavniKalkulator/src/edunova/LjubavniKalkulator.java
@@ -1,14 +1,14 @@
 package edunova;
 
 public class LjubavniKalkulator {
-	public static void main(String[] args) {
+	public void main(String[] args) {
 
 		String ime1 = "Marta";
 		String ime2 = "Manuel";
 		String ime12 = ime1 + ime2;
 
 		int k = 0;
-		int[] niz = new int[ime12.length()];
+		String[] niz = new String[ime12.length()];
 		System.out.println(ime1 + "   " + ime2);
 		for (int i = 0; i < ime12.length(); i++) {
 			k = 0;
@@ -21,57 +21,46 @@ public class LjubavniKalkulator {
 			System.out.print(k + " ");
 
 		}
+		System.out.println(rekurzija(niz));
 		System.out.println();
 		k = 0;
 	}
-		
 
-
-	    private int recursivelyCalculateLovePercent(final String letterValues) {
-	        if (letterValues.length() <= 2) {
-	            return Integer.parseInt(letterValues);
-	        }
-
-	        StringBuilder newLetterValues = new StringBuilder();
-	        final String[] letterArrayValues = letterValues.split("");
-	        for (int i = 0, j = letterArrayValues.length - 1; i <= j; i++, j--) {
-	            if (i == j) {
-	                newLetterValues.append(letterArrayValues[i]);
-	                break;
-	            }
-	            newLetterValues.append(Integer.parseInt(letterArrayValues[i]) + Integer.parseInt(letterArrayValues[j]));
-	        }
-
-	        return recursivelyCalculateLovePercent(newLetterValues.toString());
-	    }}
-
-	    
-	    
-		/*for (k = ime12.length() / 2; k > 2; k--) {
-
-			if (ime12.length() % 2 == 0) {
-				for (int i = 0; i < ime12.length() / 2; i++) {
-					int prvibroj = niz[i];
-					int drugibroj = niz[ime12.length() - 1 - i];
-					int rezultat = prvibroj + drugibroj;
-					System.out.print(rezultat + " ");
-					System.out.println();
-
-				}
-			} else {
-				for (int i = 0; i < ime12.length() / 2; i++) {
-					int prvibroj = niz[i];
-					int drugibroj = niz[ime12.length() - 1 - i];
-					int rezultat = prvibroj + drugibroj;
-					System.out.print(rezultat + " ");
-
-				}
-				int srednjibroj = niz[ime12.length() / 2];
-				System.out.print(srednjibroj);
-				System.out.println();
-
-			}
+	private int rekurzija(final String ime12) {
+		if (ime12.length() <= 2) {
+			return Integer.parseInt(ime12);
 		}
-	}
 
-}*/
+		StringBuilder newime12 = new StringBuilder();
+		final String[] novoime12 = ime12.split("");
+		for (int i = 0, j = novoime12.length - 1; i <= j; i++, j--) {
+			if (i == j) {
+				newime12.append(novoime12[i]);
+				break;
+			}
+			newime12.append(Integer.parseInt(novoime12[i]) + Integer.parseInt(novoime12[j]));
+		}
+
+		return rekurzija(novoime12.toString());
+	}
+}
+
+/*
+ * for (k = ime12.length() / 2; k > 2; k--) {
+ * 
+ * if (ime12.length() % 2 == 0) { for (int i = 0; i < ime12.length() / 2; i++) {
+ * int prvibroj = niz[i]; int drugibroj = niz[ime12.length() - 1 - i]; int
+ * rezultat = prvibroj + drugibroj; System.out.print(rezultat + " ");
+ * System.out.println();
+ * 
+ * } } else { for (int i = 0; i < ime12.length() / 2; i++) { int prvibroj =
+ * niz[i]; int drugibroj = niz[ime12.length() - 1 - i]; int rezultat = prvibroj
+ * + drugibroj; System.out.print(rezultat + " ");
+ * 
+ * } int srednjibroj = niz[ime12.length() / 2]; System.out.print(srednjibroj);
+ * System.out.println();
+ * 
+ * } } }
+ * 
+ * }
+ */
